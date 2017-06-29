3e073a0296f5a1f3b7b34454d4a23d692a8db532
Ekaterina Vaartis <TyanNN@cocaine.ninja>
2017-06-29 12:04:19 +0300

    Используем mustache-express вместо прямого рендера, мелкие изменения

--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
-const fs = require("fs")
-const express = require("express")
-const mustache = require("mustache")
-const _ = require("lodash")
-const app = express()
+const fs = require("fs"),
+      express = require("express"),
+      mustache = require("mustache"),
+      _ = require("lodash"),
+      app = express(),
+      mustacheExpress = require("mustache-express")
+
+app.use(express.static("js"))
+app.use(express.static("css"))
+app.engine("html", mustacheExpress())
+app.set("view engine", "html")
+app.set("views", "html");
 
 app.get("/", (req,res) => {
-    fs.readFile("index.html", (err, data) => {
-        if (err) throw err
-        let mData = {
-            "hours": _.range(0, 24),
-            "bars": Array(72).fill("")
-        }
-        res.send(mustache.render(data.toString(), mData))
-    })
+    let mData = {
+        "hours": _.range(0, 24),
+        "bars": Array(72).fill("")
+    }
+
+    res.render("index", mData)
 })
 
 app.listen(3000, () => {
